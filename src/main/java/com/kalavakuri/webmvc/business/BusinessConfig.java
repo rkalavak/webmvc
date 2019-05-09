ae8685da269937a9ce8f70b4cade4abfbb3a0319
rkalavak <kalavakurirenu@gmail.com>
2019-05-09 21:56:32 +0530

    Configuration and Application initialization changes.

--- src/main/java/com/kalavakuri/webmvc/business/BusinessConfig.java
+++ src/main/java/com/kalavakuri/webmvc/business/BusinessConfig.java
@@ -2,9 +2,11 @@ package com.kalavakuri.webmvc.business;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -19,11 +21,29 @@ import com.zaxxer.hikari.HikariDataSource;
 @ComponentScan
 public class BusinessConfig {
 
+	@Profile("hikariDataSource")
 	@Bean
-	public DataSource DataSource() {
+	public DataSource hikariDataSource() {
 		HikariConfig hikariConfig = new HikariConfig("/hikari.properties");
 		DataSource dataSource = new HikariDataSource(hikariConfig);
 		return dataSource;
+	}
+
+	@Profile("basicDataSource")
+	@Bean("basicDataSource")
+	public DataSource basicDataSource() {
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+
+		basicDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		basicDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		basicDataSource.setUsername("SYSTEM");
+		basicDataSource.setPassword("#knagamma1");
+		basicDataSource.setInitialSize(5);
+		basicDataSource.setMaxTotal(100);
+
+		return basicDataSource;
+
 	}
 
 	@Bean
